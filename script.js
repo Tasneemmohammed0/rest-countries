87775cc37eb5518ae27eb11689ab524cf2aedd87
Tasneem Mohammed <tasneemmohammed098@gmail.com>
2024-08-28 13:18:35 +0200

    refactor: Split fetchCountries into smaller functions

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 "use strict";
+// HTML elements
 const countriesContainer = document.querySelector(".container-countries");
-let countries = [];
+
+let countries = []; // Store all countries data
+
 // function to fetch and render countries data
 const fetchCountries = async function () {
   try {
@@ -11,26 +14,10 @@ const fetchCountries = async function () {
     if (!response.ok) throw new Error("🚨 Problem getting countries data");
 
     countries = await response.json();
-    console.log(countries);
 
     // Render all countries
     countries.forEach((country) => {
-      const markup = `
-      <div class="country-grid">
-          <img
-            class="country-img"
-            src="${country.flags.png}"
-            alt="${country.name.common}-flag"
-          />
-          <h3 class="country-name">${country.name.common}</h3>
-          <div class="country-info">
-            <p><strong>Population:</strong>${country.population}</p>
-            <p><strong>Region:</strong>${country.region}</p>
-            <p><strong>Capital:</strong>${
-              country.capital ? country.capital[0] : "N/A"
-            }</p>
-          </div>
-        </div>`;
+      const markup = generateMarkup(country);
       countriesContainer.insertAdjacentHTML("beforeend", markup);
     });
   } catch (err) {
@@ -38,4 +25,26 @@ const fetchCountries = async function () {
   }
 };
 
+// function to return country grid markup
+const generateMarkup = function(country) {
+  return = `
+  <div class="country-grid">
+      <img
+        class="country-img"
+        src="${country.flags.png}"
+        alt="${country.name.common}-flag"
+      />
+      <h3 class="country-name">${country.name.common}</h3>
+      <div class="country-info">
+        <p><strong>Population:</strong>${country.population}</p>
+        <p><strong>Region:</strong>${country.region}</p>
+        <p><strong>Capital:</strong>${
+          country.capital ? country.capital[0] : "N/A"
+        }</p>
+      </div>
+    </div>`;
+
+}
+
+// Calling the function to fetch and render countries
 fetchCountries();
